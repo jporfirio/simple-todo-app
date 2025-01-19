e928d342d0b4b3f7a960148e5309a65d33c3e019
Jeann Porfírio <jeannporfirio@gmail.com>
2025-01-19 14:16:27 -0300

    header reafactor

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,10 @@
 import React from "react";
 
-const Header = () => {
-  const headerStyle = {
-    padding: "20px 0",
-    lineHeight: "2em"
-  };
-  return (
-    <header style={headerStyle}>
-      <h1 style={{ fontSize: "25px", marginBottom: "15px" }}>
-        Simple Todo App
-      </h1>
-      <p style={{ fontSize: "19px" }}>
-        Please add to-dos item(s) through the input field
-      </p>
-    </header>
-  );
-};
+const Header = () => (
+  <header className="header">
+    <h1>Simple Todo App</h1>
+    <p>Please add to-dos item(s) through the input field</p>
+  </header>
+);
 
 export default Header;
